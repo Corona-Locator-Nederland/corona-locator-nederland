d8bdbc5189b30b7662fcf4c0af6e504371b4c834
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-01-08 15:31:28 +0100

    knack update

--- knack.py
+++ knack.py
@@ -1,16 +1,28 @@
+from tqdm.notebook import tqdm as progress
+from urllib.request import urlopen
 from munch import Munch
-import knackpy
 from jsonpath import JSONPath
+import json
+from retry import retry
+from requests import HTTPError
+import time
+import requests
+from time import sleep
+
+def is_running_from_ipython():
+  from IPython import get_ipython
+  return get_ipython() is not None
+print('notebook', is_running_from_ipython())
 
 class Knack:
   def __init__(self, app_id, api_key):
+    self.app_id = app_id
+    self.api_key = api_key
     self.headers = {
       'X-Knack-Application-Id': app_id,
       'X-Knack-REST-API-KEY': api_key,
-      'content-type': 'application/json',
     }
-    self.app = knackpy.App(app_id=os.environ['KNACK_APP_ID'],  api_key=os.environ['KNACK_API_KEY'], tzinfo='Europe/Amsterdam')
-    with urlopen(f'https://loader.knack.com/v1/applications/{os.environ["KNACK_APP_ID"]}') as response:
+    with urlopen(f'https://loader.knack.com/v1/applications/{app_id}') as response:
       self.metadata = json.load(response)
 
   def find(self, path):
@@ -18,35 +30,60 @@ class Knack:
     assert len(found) == 1, (len(found), path)
     return self.munch(found[0])
 
-  def create(self, object_key, data):
+  def _check(self, res):
+    if res.status_code >= 200 and res.status_code < 300: return res
+    try:
+      msg = res.json()
+      if type(msg) == dict and 'errors' in msg: raise ValueError(json.dumps(msg['errors']))
+    except HTTPError as e:
+      raise e
+    res.raise_for_result()
+
+  @retry(HTTPError, delay=1, tries=2) # absolutely ridiculous
+  def _create(self, object_key, data):
+    #curl = ' '.join(['-H ' + shlex.quote(f'{k}: {v}') for k, v in self.headers.items()])
+    #curl = f'curl -X POST https://api.knack.com/v1/objects/{object_key}/records {curl} --data-binary {shlex.quote(json.dumps(data))}'
+    #print(curl)
+    #curl = subprocess.run(curl, shell=True)
+    #if curl.returncode != 0: raise ValueError(str(curl.returncode))
+    #return
+
     url = f'https://api.knack.com/v1/objects/{object_key}/records'
-    res = requests.put(url=url, data=data, headers=self.headers)
+    self._check(requests.post(url=url, json=data, headers=self.headers))
 
-  def modify(self, object_key, record_id, data):
+  @retry(HTTPError, delay=1, tries=2) # absolutely ridiculous
+  def _update(self, object_key, record_id, data):
     url = f'https://api.knack.com/v1/objects/{object_key}/records/{record_id}'
-    res = requests.put(url=url, data=data, headers=self.headers)
+    self._check(requests.put(url=url, json=data, headers=self.headers))
 
-  def delete(self, object_key, record_id):
+  @retry(HTTPError, delay=1, tries=2) # absolutely ridiculous
+  def _delete(self, object_key, record_id):
     url = f'https://api.knack.com/v1/objects/{object_key}/records/{record_id}'
-    res = requests.delete(url=url, headers=self.headers)
+    self._check(requests.delete(url=url, headers=self.headers))
 
-  def get(self, object_key):
-    url = f'https://api.knack.com/v1/objects/{object_key}/records'
+  def _getall(self, object_key):
+    url = f'https://api.knack.com/v1/objects/{object_key}/records?rows_per_page=1000'
     records = []
     page = 1
     while True:
-      res = requests.get(url=f'{url}?page={page}', headers=self.headers).json()
+      res = self._check(requests.get(url=f'{url}&page={page}', headers=self.headers)).json()
       records += res['records']
-      if res['current_page'] != res['total_pages']:
+      if res['current_page'] != res['total_pages'] and res['total_pages'] != 0: # what the actual...
         page += 1
       else:
         break
+    # echt mensen dit geloof je niet.
+    for rec in records:
+      for k, v in list(rec.items()):
+        if not k.endswith('_raw'):
+          continue
+        if type(v) == dict and 'date' in v:
+          v = v['iso_timestamp'].replace('T00:00:00.000Z', '')
+        rec[k.replace('_raw', '')] = v
+        rec.pop(k)
+
     return self.munch(records)
 
-#    @retry(HTTPError, delay=1, tries=10) # absolutely ridiculous
-#    def record(self, method, data, obj):
-#      self.app.record(method=method, data=data, obj=obj)
-  
   def munch(self, records):
     if type(records) == list:
       return [ Munch.fromDict(rec) for rec in records ]
@@ -55,35 +92,23 @@ class Knack:
     else:
       raise ValueError(f'Unexpected type {str(type(records))}')
 
-  def update(self, scene, view, df):
-    print('x')
+  def update(self, scene, view, df, verbose=False):
     view = self.find(f'$.application.scenes[?(@.name=={json.dumps(scene)})].views[?(@.name=={json.dumps(view)})]')
     source = view.source.object
     obj = self.find(f'$.application.objects[?(@.key=="{source}")]')
     mapping = { field.name: field.key for field in obj.fields }
-    id_field = obj.identifier
 
-    #ist = [dict(rec) for rec in self.app.get(source)]
-    ist = self.get(obj.key)
-    soll = df.rename(columns=mapping).to_dict('records')
-    keys = self.munch({
-      'ist': { rec[id_field]: rec.id for rec in ist},
-      'soll': [rec[id_field] for rec in soll],
-    })
-    print(keys)
+    soll = { rec[obj.identifier]: Munch(record=rec, key=rec[obj.identifier]) for rec in df.rename(columns=mapping).to_dict('records') }
+    ist = self._getall(obj.key)
+    # fetch these separately as the key does not need to be unique?
+    delete = [rec.id for rec in ist if rec.get(obj.identifier) not in soll]
+    ist  = { rec[obj.identifier]: Munch(record=rec, key=rec[obj.identifier]) for rec in ist if not rec.id in delete }
+   
+    for record_id in progress(delete, desc='deleting obsolete records'):
+      self._delete(object_key=obj.key, record_id=record_id)
 
-    with timer() as t:
-      for rec in ist:
-        if rec[id_field] not in keys.soll:
-          print('deleting', rec[id_field])
-          #self.delete(method='delete', data=rec, obj=obj['key'])
-          self.delete(object_key=obj.key, record_id=rec.id)
-      for rec in soll:
-        if record_id := keys.ist.get(rec[id_field]):
-          print('updating', rec[id_field])
-          #self.record(method='update', data={**rec, 'id': keys['ist'][rec[id_field]]}, obj=obj['key'])
-          self.modify(object_key=obj.key, record_id=record_id, data=rec)
-        else:
-          print('creating', rec[id_field])
-          self.create(objject_key=obj.key, data=rec)
-          #self.record(method="create", data=rec, obj=obj['key'])
+    for data in progress([rec.record for rec in soll.values() if rec.key not in ist], desc='creating records'):
+      self._create(object_key=obj.key, data=data)
+
+    for record_id, data in progress([(ist[rec.key].record.id, rec.record) for rec in soll.values() if rec.key in ist and {**rec.record, 'id': None} != {**ist[rec.key].record, 'id': None}], desc='updating records'):
+      self._update(object_key=obj.key, record_id=record_id, data=data)
