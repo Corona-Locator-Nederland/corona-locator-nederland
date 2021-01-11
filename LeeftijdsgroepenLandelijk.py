a4a0b9a39f861ecf58a47c4ebb6e7f36145da3f4
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-01-11 22:01:06 +0100

    cleanup

--- LeeftijdsgroepenLandelijk.py
+++ LeeftijdsgroepenLandelijk.py
@@ -2,47 +2,8 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 from IPython import get_ipython
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
+get_ipython().run_line_magic('run', 'setup')
 
-# %%
-from dotenv import load_dotenv, find_dotenv
-dot_env = find_dotenv()
-if dot_env == '': dot_env = find_dotenv(filename='dot.env')
-load_dotenv(dot_env, override=True)
-
-from ruamel.yaml import YAML
-yaml = YAML(typ='safe')
-
-from urllib.request import urlopen
-from urllib.request import urlretrieve
-import pandas as pd
-import numpy as np
-import json
-import datetime
-import requests
-import json
-import math
-import os
-import seaborn as sns
-import glob
-from dateutil.parser import parse as parsedate
-import functools
-import matplotlib.colors as colors
-#from types import SimpleNamespace
-
-if 'KNACK_APP_ID' in os.environ:
-  from knack import Knack
-  knack = Knack(app_id = os.environ['KNACK_APP_ID'], api_key = os.environ['KNACK_API_KEY'])
-else:
-  knack = None
-
-def run(*args):
-  if len(args) == 1 and callable(args[0]):
-    return args[0]()
-  else:
-    print(*args)
-    return lambda func: func()
 # %% [markdown]
 # Download de RIVM data als die nieuwer is dan wat we al hebben (gecached want de download van RIVM is *zeer* traag)
 
@@ -73,18 +34,6 @@ def cell():
 # https://www.cbs.nl/nl-nl/onze-diensten/open-data/open-data-v4/snelstartgids-odata-v4
 @run('leeftijdsgroepen: download demografische data van CBS')
 def cell():
-  def get_odata(target_url):
-    data = pd.DataFrame()
-    while target_url:
-      r = requests.get(target_url).json()
-      data = data.append(pd.DataFrame(r['value']))
-
-      if '@odata.nextLink' in r:
-        target_url = r['@odata.nextLink']
-      else:
-        target_url = None
-    return data
-
   def roundup(x):
     return int(math.ceil(x / 10.0)) * 10
   def rounddown(x):
---
+++ setup.py
@@ -0,0 +1,57 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+from dotenv import load_dotenv, find_dotenv
+dot_env = find_dotenv()
+if dot_env == '': dot_env = find_dotenv(filename='dot.env')
+load_dotenv(dot_env, override=True)
+
+from ruamel.yaml import YAML
+yaml = YAML(typ='safe')
+
+from urllib.request import urlopen
+from urllib.request import urlretrieve
+import pandas as pd
+import numpy as np
+import json
+import datetime
+import requests
+import json
+import math
+import os
+import seaborn as sns
+import glob
+from dateutil.parser import parse as parsedate
+import functools
+import matplotlib.colors as colors
+#from types import SimpleNamespace
+
+if 'KNACK_APP_ID' in os.environ:
+  from knack import Knack
+  knack = Knack(app_id = os.environ['KNACK_APP_ID'], api_key = os.environ['KNACK_API_KEY'])
+else:
+  knack = None
+
+def run(*args):
+  if len(args) == 1 and callable(args[0]):
+    return args[0]()
+  else:
+    print(*args)
+    return lambda func: func()
+
+def get_odata(url):
+  data = pd.DataFrame()
+  while url:
+    r = requests.get(url).json()
+    data = data.append(pd.DataFrame(r['value']))
+
+    if '@odata.nextLink' in r:
+      url = r['@odata.nextLink']
+    else:
+      url = None
+  return data
+
