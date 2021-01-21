b733babab2213d5866372a467e27ed40297c643d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-01-21 12:53:23 +0100

    comment & cleanup

--- Regio.py
+++ Regio.py
@@ -10,11 +10,9 @@ if 'GSHEET' in os.environ:
   sh = gc.open_by_key(os.environ['GSHEET'])
   ws = sh.get_worksheet(0)
 
+# sorteert de kolommen alfabetisch, is makkelijker visueel te debuggen.
 def sortcolumns(df):
   return df[sorted(df.columns)]
-
-def orderedprefix(df, prefix, exclude=[]):
-  return df[prefix + [col for col in df.columns if col not in prefix and col not in exclude]]
 
 def publish(df):
   display(df.head())
@@ -30,6 +28,7 @@ def publish(df):
 @run('regio: load regios en hun basisgegevens')
 def cell():
   global gemeenten
+  # rename de kolommen naar "Naam" + "NaamCode" voor makkelijker uniforme data bewerking
   gemeenten = pd.read_csv('gemeenten.csv').rename(columns={
     'Code': 'GemeenteCode',
     'Naam': 'Gemeente',
@@ -37,13 +36,16 @@ def cell():
     'GGD regio': 'GGDregio',
     'Landcode': 'LandCode',
   })
+  # niet nodig want die voegen we vanzelf toe bij de per-type constructie van de cijfers
   del gemeenten['Type']
   
   global regiocodes
   regiocodes = pd.read_csv('regiocodes.csv')
+  # sluit aan bij de uniforme naamgeving van hierboven
   regiocodes = regiocodes.rename(columns={'Landcode': 'LandCode'})
   regiocodes.loc[regiocodes.Type == 'GGD', 'Type'] = 'GGDregio'
   
+  # voeg de regiocodes toe aan de gemeenten tabel voor makkelijker uniforme data bewerking
   for regiotype in ['GGDregio', 'Provincie', 'Landsdeel', 'Schoolregio']:
     gemeenten = gemeenten.merge(
       regiocodes[regiocodes.Type == regiotype][['LandCode', 'Regio', 'Code']].rename(columns={'Code': regiotype + 'Code', 'Regio': regiotype}),
@@ -55,7 +57,8 @@ def cell():
     how='left',
     on='LandCode'
   )
-  
+
+  # lege regel voor GM0000
   for regiotype, prefix in [('GGDregio', 'GG'), ('Veiligheidsregio', 'VR'), ('Provincie', 'PV'), ('Landsdeel', 'LD'), ('Schoolregio', 'SR')]:
     gemeenten.loc[gemeenten.GemeenteCode == 'GM0000', regiotype] = ''
     gemeenten.loc[gemeenten.GemeenteCode == 'GM0000', f'{regiotype}Code'] = f'{prefix}00'
@@ -79,22 +82,27 @@ def cell():
   gemeenten.loc[gemeenten.Personen == 0, 'Personen'] = gemeenten.BevolkingAanHetBeginVanDePeriode
   del gemeenten['BevolkingAanHetBeginVanDePeriode']
   gemeenten = sortcolumns(gemeenten)
-  
+
+  # prepareer een RIVM dataset
   def prepare(dataset, day):
     df = rivm_cijfers(dataset, day)
+    # hernoem kolommen voor makkelijker uniforme data bewerking
     df[['GemeenteCode', 'VeiligheidsregioCode', 'Veiligheidsregio']] = df[['Municipality_code', 'Security_region_code', 'Security_region_name']]
     df['GemeenteCode'] = df['GemeenteCode'].fillna('GM0000')
   
+    # voeg regiocodes to aan elke regel in de dataset
     for regiotype in ['GGDregio', 'Provincie', 'Landsdeel', 'Schoolregio']:
       df = df.merge(gemeenten[['GemeenteCode', f'{regiotype}Code']].drop_duplicates(), on='GemeenteCode')
     df['LandCode'] = 'NL'
   
-    # knip de tijd van de datum af
+    # knip de tijd van de datum af, en stop hem in 'Today' (referentiepunt metingen)
     df['Today'] = pd.to_datetime(df.Date_of_report.str.replace(' .*', '', regex=True))
   
+    # zet 'Date' naar de bij de betreffende dataset horende meetdatum-kolom
     for when in ['Date_of_statistics', 'Date_of_publication']:
       if when in df:
         df['Date'] = pd.to_datetime(df[when])
+        # en direct maar weken terug, die hebben we vaker nodig
         df['WekenTerug'] = ((df.Today - df.Date) / np.timedelta64(7, 'D')).astype(np.int)
     return sortcolumns(df)
 
@@ -103,14 +111,18 @@ def cell():
   ziekenhuisopnames = prepare('COVID-19_ziekenhuisopnames', 0)
   ziekenhuisopnames_1 = prepare('COVID-19_ziekenhuisopnames', 1)
 
-# %%
+# %% Ondersteunde code voor de regio berekeningen
 def groupregio(regiotype):
+  """
+    Groepeer de gemeenten tabel op gegeven regiotypen en sommeer personen/oppervlakte
+  """
   global gemeenten
 
   grouping = [ f'{regiotype}Code', regiotype]
   if regiotype != 'Land':
     grouping += [ 'LandCode' ]
 
+  # deze kolommen willen we voor de resultset, ook al zijn ze voor alles behalve 'Gemeente' leeg
   columns = [
     'GGDregio',
     'Veiligheidsregio',
@@ -122,43 +134,70 @@ def groupregio(regiotype):
   ]
 
   if regiotype == 'Gemeente':
+    # hier hoeven we niets te doen dan de juiste kolommen te selecteren
     df = gemeenten[grouping + columns + ['Personen', 'Opp land km2']].rename(columns={'Gemeente': 'Naam', 'GemeenteCode': 'Code'})
   else:
     df = (gemeenten[gemeenten.GemeenteCode != 'GM0000']
+      # groupeer op regiotype, sommeer oppervlakte en personen
       .groupby(grouping).agg({ 'Personen': 'sum', 'Opp land km2': 'sum' })
       .reset_index()
+      # standardiseerd de 'Naam' en 'Code' kolommen zodat ze klaar zijn voor output.
       .rename(columns={regiotype: 'Naam', f'{regiotype}Code': 'Code'})
+      # voeg de lege kolommen toe
       .assign(**{ col: '' for col in columns })
     )
     if regiotype == 'Land':
       df['Land'] = df['Naam']
       df['LandCode'] = df['Code']
+  # voeg het regiotype toe in de Type kolom
   return df.assign(Type=regiotype)
 
 def sumcols(df, regiotype, columns):
+  """
+    groepeer en sommeer deceased/admission/positive
+  """
   regiotype_code = f'{regiotype}Code'
   return (df
+    # groepeer op regiotype en selecteer de gewenste kolommen
     .groupby([regiotype_code])[list(columns.keys())]
+    # sommeer
     .sum()
+    # rename naar gewenste output kolommen
     .rename(columns=columns)
   )
 
 def histcols(df, regiotype, maxweeks, column, colors=False, highestweek=False):
+  """
+    voeg week historie toe:
+    - regiotype
+    - maxweeks = hoeveel weken
+    - column = deceased/admission/positive naam => output kolom
+    - colors = toevoegen schaalverdeling kleuren
+    - highestweek = toevoegen op welke week het maximum was behaald
+  """
+  # in principe zouden we kunnen groeperen/sommeren op meerdere kolommen tegelijk maar dan worden colors en highestweek weer heel complex
   assert len(column) == 1
   label = list(column.values())[0]
   datacolumn = list(column.keys())[0]
   regiotype_code = f'{regiotype}Code'
 
+  # knip alle data van >= maxweeks eruit
   df = df[df.WekenTerug < maxweeks]
+
+  # als we schalen naar 100.000, voor hoeveel telt elke melding dan mee
   if 'scale' in df:
     df = df.assign(count=df[datacolumn] * df.scale)
   else:
     df = df.assign(count=df[datacolumn])
 
   df = (df
+    # groepeer op reguitype en wekenterug
     .groupby([regiotype_code, 'WekenTerug'])[datacolumn]
+    # optellen (de aantallen zijn eventueel hierboven al geschaald)
     .sum()
+    # maak de week nummers kolommen
     .unstack(level=-1)
+    # en vul de kolommen uit zodat als een week helemaal geen meldingen heeft dat die niet weg blijft maar 0 bevat
     .reindex(columns=np.arange(maxweeks), fill_value=0)
   )
 
@@ -167,10 +206,12 @@ def histcols(df, regiotype, maxweeks, column, colors=False, highestweek=False):
   if highestweek:
     merges.append(df.idxmax(axis=1).to_frame().rename(columns={0: f'{label} hoogste week'}))
 
+  # hernoem de kolommen van de getallen die ze nu zijn
   df.columns = [f'{label} w{-col}' for col in df.columns.values]
 
   # must be done before highestweek is merged but after the columns are renamed
   if colors:
+    # kleurkolommen zijn waarde van de week gedeeld door het maximum over de weken heen
     merges.append((df.divide(df.max(axis=1), axis=0) * 1000).rename(columns={col:col.replace(' w', ' cw') for col in df}))
 
   for extra in merges:
@@ -181,24 +222,35 @@ def histcols(df, regiotype, maxweeks, column, colors=False, highestweek=False):
   return df
 
 def collect(regiotype):
+  """
+    verzamel alle kolommen voor gegeven regiotype
+  """
   regiotype_code = f'{regiotype}Code'
 
   global aantallen, ziekenhuisopnames, ziekenhuisopnames_1
 
+  # sommeer Total_reported en Deceased voor gegeven regiotype
   pos_dec = sumcols(aantallen, regiotype, {'Total_reported':'Positief getest', 'Deceased':'Overleden'})
+  # toename is precies hetzelfde maar dan alleen voor de metingen van 'vandaag'
   pos_dec_delta = sumcols(
     aantallen[aantallen.Date == aantallen.Today],
     regiotype,
     {'Total_reported':'Positief getest (toename)', 'Deceased':'Overleden (toename)'}
   )
+  # sommeer Hospital_admission voor gegeven regiotype
   admissions = sumcols(ziekenhuisopnames, regiotype, {'Hospital_admission':'Ziekenhuisopname'})
+  # sommeer Hospital_admission van 'vorige dag' voor gegeven regiotype
   admissions_1 = sumcols(ziekenhuisopnames_1, regiotype, {'Hospital_admission':'Ziekenhuisopname_1'})
+  # voeg ze bij elkaar en trek ze van elkaar af
   admissions_delta = admissions.merge(admissions_1, how='left', on=regiotype_code)
   admissions_delta['Ziekenhuisopname (toename)'] = admissions_delta.Ziekenhuisopname - admissions_delta.Ziekenhuisopname_1
+  # niet meer nodig en vervuilen anders het resultaat
   del admissions_delta['Ziekenhuisopname']
   del admissions_delta['Ziekenhuisopname_1']
 
+  # groupeer de gemeenten op gegeven regiotype
   df = (groupregio(regiotype)
+    # en voeg de berekende kolommen toe
     .merge(pos_dec, how='left', left_on='Code', right_index=True)
     .merge(admissions, how='left', left_on='Code', right_index=True)
     .merge(pos_dec_delta, how='left', left_on='Code', right_index=True)
@@ -214,6 +266,7 @@ def collect(regiotype):
   df['Positief getest percentage'] = (df['Positief getest'] / df.Personen).replace(np.inf, 0)
   df['Positief getest per km2'] = (df['Positief getest'] / df['Opp land km2']).replace(np.inf, 0)
 
+  # weekhistories
   maxweeks = 26
   df = (df
     .merge(histcols(
@@ -247,21 +300,23 @@ def collect(regiotype):
 @run('regio: load verschillende aggregatie niveaus')
 def cell():
   global regios
+  # verzamel de data voor de gegeven regiotypes en plak ze onder elkaar
   regios = pd.concat([
     collect(regiotype)
     for regiotype in
     [ 'Gemeente', 'GGDregio', 'Veiligheidsregio', 'Provincie', 'Landsdeel', 'Schoolregio', 'Land' ]
   ])
+  # maak de kolommen leeg voor GM0000
   regios.loc[regios.Code == 'GM0000']['VeiligheidsregioCode'] = ''
+  # hernoem de eerder geuniformeerde kolomen terug naar de gewenste output
   regios = regios.rename(columns={
     'LandCode': 'Landcode',
     'VeiligheidsregioCode': 'Veiligheidsregio Code',
     'GGDregio': 'GGD regio'
   })
 
-
-
 # %%
+# load de gewenste kolom volgorde uit een file en publiceer
 order = pd.read_csv('columnorder.csv')
 publish(regios[order.columns.values].fillna(0))
 
