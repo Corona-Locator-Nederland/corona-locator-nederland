e81dea5c9a7c0a8addd12be6536bc7141dd52403
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-01-31 23:59:22 +0100

    cumsum

--- DagOverzicht.py
+++ DagOverzicht.py
@@ -8,11 +8,18 @@ import itertools
 @run('regio: load regios en hun basisgegevens')
 def cell():
   df = RIVM.json('COVID-19_prevalentie')
-  display(df.head())
   df = RIVM.json('COVID-19_reproductiegetal')
-  display(df.head())
-  df = CBS.leeftijdsgroepen_bevolking()
-  display(df.head())
+  #df = CBS.leeftijdsgroepen_bevolking()
   df = LCPS.csv('covid-19')
+  df = RIVM.csv('COVID-19_casus_landelijk')
+
+  df = RIVM.csv('COVID-19_aantallen_gemeente_per_dag')
+  df = (df
+        .groupby(['Date_of_publication'])['Total_reported']
+        .sum()
+        .reset_index()
+  )
+  df['cumsum'] = df['Total_reported'].cumsum()
   display(df.head())
+
 # %%
