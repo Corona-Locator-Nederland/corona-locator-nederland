4f1c5bc3c804d48db6cac292b33ee2f8b1673c6e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-02-05 21:48:35 +0100

    DagOverzicht.py

--- DagOverzicht.py
+++ DagOverzicht.py
@@ -7,38 +7,48 @@ import itertools
 # %%
 @run('set up base frame + overleden + positief getest')
 def cell():
-  #df = RIVM.json('COVID-19_prevalentie')
-  #df = RIVM.json('COVID-19_reproductiegetal')
-  #df = CBS.leeftijdsgroepen_bevolking()
-  #df = LCPS.csv('covid-19')
-  #df = RIVM.csv('COVID-19_casus_landelijk')
-
-  ag = RIVM.csv('COVID-19_aantallen_gemeente_per_dag')
-  ag = ag.rename(columns={
+  df = RIVM.csv('COVID-19_aantallen_gemeente_per_dag').rename(columns={
     'Total_reported': 'Positief getest',
     'Deceased': 'Overleden',
     'Date_of_publication': 'Datum',
     'Date_of_report': 'Today',
   })
-  ag['Datum'] = pd.to_datetime(ag.Datum.str.replace(' .*', '', regex=True))
-  ag['Today'] = pd.to_datetime(ag.Today.str.replace(' .*', '', regex=True))
+  df['Datum'] = pd.to_datetime(df.Datum.str.replace(' .*', '', regex=True))
+  df['Today'] = pd.to_datetime(df.Today.str.replace(' .*', '', regex=True))
 
   global dagoverzicht
-  dagoverzicht = pd.DataFrame(index=pd.date_range(start='2020-02-27', end=ag.Today.max()))
+  dagoverzicht = pd.DataFrame(index=pd.date_range(start='2020-02-27', end=df.Today.max()))
   dagoverzicht.index.name='Key'
   dagoverzicht['Datum'] = dagoverzicht.index.strftime('%Y-%m-%d')
   dagoverzicht['LandCode'] = 'NL'
-  #display(dagoverzicht)
 
-  ag = (ag
+  df = (df
         .groupby(['Datum'])
         .agg({'Positief getest': 'sum', 'Overleden': 'sum'})
   )
-  ag['Positief getest'] = ag['Positief getest'].cumsum()
-  ag['Overleden'] = ag['Overleden'].cumsum()
-  dagoverzicht = dagoverzicht.merge(ag, how='left', left_index=True, right_index=True)
+  df['Positief getest'] = df['Positief getest'].cumsum()
+  df['Overleden'] = df['Overleden'].cumsum()
+  dagoverzicht = dagoverzicht.merge(df, how='left', left_index=True, right_index=True)
   display(dagoverzicht)
 
+# %%
+@run('ziekenhuisopnames')
+def cell():
+  df = RIVM.csv('COVID-19_ziekenhuisopnames').rename(columns={
+    'Hospital_admission': 'Ziekenhuisopnames',
+    'Date_of_statistics': 'Datum',
+  })
+  df['Datum'] = pd.to_datetime(df.Datum.str.replace(' .*', '', regex=True))
+  df = (df
+        .groupby(['Datum'])
+        .agg({'Ziekenhuisopnames': 'sum'})
+  )
+  display(df)
+  global dagoverzicht
+  dagoverzicht = dagoverzicht.merge(df, how='left', left_index=True, right_index=True)
+  dagoverzicht['Ziekenhuisopnames'] = dagoverzicht['Ziekenhuisopnames'].fillna(0).astype(int).cumsum()
+
+  display(dagoverzicht)
 # %%
 @run('publish')
 def cell():
