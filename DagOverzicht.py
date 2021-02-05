263bf96738406878b5a9606d9664e353d2cfa508
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-02-05 21:34:39 +0100

    DagOverzicht key colom + overleden + positief

--- DagOverzicht.py
+++ DagOverzicht.py
@@ -5,29 +5,45 @@ get_ipython().run_line_magic('run', 'setup')
 import itertools
 
 # %%
-@run('regio: load regios en hun basisgegevens')
+@run('set up base frame + overleden + positief getest')
 def cell():
-  df = RIVM.json('COVID-19_prevalentie')
-  df = RIVM.json('COVID-19_reproductiegetal')
+  #df = RIVM.json('COVID-19_prevalentie')
+  #df = RIVM.json('COVID-19_reproductiegetal')
   #df = CBS.leeftijdsgroepen_bevolking()
-  df = LCPS.csv('covid-19')
-  df = RIVM.csv('COVID-19_casus_landelijk')
+  #df = LCPS.csv('covid-19')
+  #df = RIVM.csv('COVID-19_casus_landelijk')
 
-  df = RIVM.csv('COVID-19_aantallen_gemeente_per_dag')
-  df = df.rename(columns={
+  ag = RIVM.csv('COVID-19_aantallen_gemeente_per_dag')
+  ag = ag.rename(columns={
     'Total_reported': 'Positief getest',
     'Deceased': 'Overleden',
     'Date_of_publication': 'Datum',
+    'Date_of_report': 'Today',
   })
+  ag['Datum'] = pd.to_datetime(ag.Datum.str.replace(' .*', '', regex=True))
+  ag['Today'] = pd.to_datetime(ag.Today.str.replace(' .*', '', regex=True))
 
-  df = (df
+  global dagoverzicht
+  dagoverzicht = pd.DataFrame(index=pd.date_range(start='2020-02-27', end=ag.Today.max()))
+  dagoverzicht.index.name='Key'
+  dagoverzicht['Datum'] = dagoverzicht.index.strftime('%Y-%m-%d')
+  dagoverzicht['LandCode'] = 'NL'
+  #display(dagoverzicht)
+
+  ag = (ag
         .groupby(['Datum'])
         .agg({'Positief getest': 'sum', 'Overleden': 'sum'})
-        .reset_index()
   )
-  df['Key'] = df['Datum']
-  df['LandCode'] = 'NL'
-  df['cumsum'] = df['Positief getest'].cumsum()
-  display(df.head())
+  ag['Positief getest'] = ag['Positief getest'].cumsum()
+  ag['Overleden'] = ag['Overleden'].cumsum()
+  dagoverzicht = dagoverzicht.merge(ag, how='left', left_index=True, right_index=True)
+  display(dagoverzicht)
 
+# %%
+@run('publish')
+def cell():
+  global dagoverzicht
+
+  os.makedirs('artifacts', exist_ok = True)
+  dagoverzicht.to_csv('artifacts/DagOverzicht.csv', index=True)
 # %%
