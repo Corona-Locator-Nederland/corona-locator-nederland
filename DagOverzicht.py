c0f4ec0c42ee5f929a9ac815f328275b5188a629
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-02-14 15:50:22 +0100

    date range from NICE

--- DagOverzicht.py
+++ DagOverzicht.py
@@ -37,7 +37,7 @@ def cell():
   # base date range uit NICE
   global dagoverzicht
   # maak leeg dataframe met een rij voor elke dag van 2020-02-27 tm Date_of_publication
-  dagoverzicht = pd.DataFrame(index=pd.date_range(start='2020-02-27', end=df.date.max()))
+  dagoverzicht = pd.DataFrame(index=pd.date_range(start='2020-02-27', end=df.index.max()))
   # noem de index Key
   dagoverzicht.index.name='Key'
   # vul de datum kolom
