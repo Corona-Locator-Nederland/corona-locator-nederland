d136e79d7006e10a0eb9c4c993fbe5597bd1f767
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-02-25 10:16:51 +0100

    simple name when possible

--- knack.py
+++ knack.py
@@ -293,7 +293,7 @@ class Knack:
       prefix += os.environ['GITHUB_RUN_NUMBER']
       prefix += '> '
 
-    if nb := os.environ.get('NOTEBOOK'):
+    if nb := os.environ.get('NOTEBOOK') and nb != objectName:
       prefix += f'*{nb}.{objectName}* '
     else:
       prefix += f'*{objectName}* '
