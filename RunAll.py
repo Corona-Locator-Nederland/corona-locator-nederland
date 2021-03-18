10d9f07b3305b337ed8186d8f4af1dff0a595db3
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2021-03-18 19:16:20 +0100

    run-all

--- RunAll.py
+++
@@ -1,6 +0,0 @@
-# %%
-from IPython import get_ipython
-from IPython.core.display import display
-ipy = get_ipython()
-for nb in ['Dagoverzicht', 'LeeftijdsgroepenLandelijk', 'Regio']:
-  ipy.run_line_magic('run', nb + '.ipynb')
